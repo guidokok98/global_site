3516126bfc8c765ae822417c5a1f52379c7cf675
Guido Kok <guidokok@slamortho.com>
2023-04-28 11:20:37 +0200

    removed ser.close caused problems

--- mh_z19c.py
+++ mh_z19c.py
@@ -24,21 +24,12 @@ class mh_z19c:
     serial_dev = None
     baudrate = 9600
     retry_count = 3
-    ser = None
     
     #when new class object is made, set temp,humi and press oversampling on 8 and IIR filter on 3
     def __init__(self):
         partial_serial_dev = 'ttyS0'
         serial_dev = '/dev/%s' % partial_serial_dev
-        
-    def connect_serial(self):
-        self.ser = serial.Serial(self.serial_dev,
-                            baudrate=self.baudrate,
-                            bytesize=serial.EIGHTBITS,
-                            parity=serial.PARITY_NONE,
-                            stopbits=serial.STOPBITS_ONE,
-                            timeout=1.0)
-        return self.ser
+
   
     def checksum(self, array):
       csum = sum(array) % 0x100
@@ -53,7 +44,6 @@ class mh_z19c:
         for retry in range(self.retry_count):
           result=ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
           s=ser.read(9)
-          ser.close()
         if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86 and ord(self.checksum(s[1:-1])) == s[-1]:
           return s[2]*256 + s[3]
       except:
